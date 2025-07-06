c1730aa0fd3774c28d53535663b80c4e5948b4a8
Xaver Robrecht <167310053+xaver-robrecht@users.noreply.github.com>
2025-07-06 16:04:30 +0200

    added Bechmark

--- README.md
+++ README.md
@@ -1,10 +1,46 @@
 # PatchCore
 
-This repository contains a simple implementation of the PatchCore anomaly detection algorithm, which is based on the paper "PatchCore: Efficient Patch-based Outlier Detection" by R. Schneider et al. (2022). The implementation is designed to be straightforward and easy to use, with a focus on clarity and simplicity.
-Also the PatchCore classifier is implemented as custom torch module, enabling easy integration into PyTorch-based workflows or export to production environments via ONNX or similar frameworks.
+This repository contains a simple implementation of the PatchCore anomaly detection algorithm, based on the paper *"PatchCore: Efficient Patch-based Outlier Detection"* by R. Schneider et al. (2022). The implementation is designed to be straightforward and easy to use, with a focus on clarity and simplicity.
 
-For a reference on how to use the package, please see `example.py`. This script demonstrates typical usage patterns and can serve as a starting point for your own applications.
+The PatchCore classifier is implemented as a custom Torch module, enabling seamless integration into PyTorch-based workflows and facilitating export to production environments via ONNX or similar frameworks.
 
-I plan to eventually publish this package on PyPI for easier usage. Also, as currently the scikit learn and scikit image libraries are only used in evaluating the benchmark asn generating the segmentations i will eventually remove wthem and add custom implementations to compute the AUROC score and find contours.
+For usage examples, please refer to `example.py`. This script demonstrates typical usage patterns and can serve as a starting point for your own applications.
 
-Benchmark results on the MVTecAD dataset will be uploaded soon.
+I plan to eventually publish this package on PyPI for easier accessibility. As the `scikit-learn` and `scikit-image` libraries are currently only used for benchmarking and segmentation generation, I plan to remove them and implement custom methods for computing the AUROC score and detecting contours.
+
+---
+
+# AUROC Score Benchmark Results on the MVTecAD Dataset
+
+I ran a few test runs with the following settings:
+- Nearest neighbors set to 1 (equivalent to no reweighting)
+- Patch size set to 3
+- Subsampling at 1%
+- Feature dimensions as proposed in the [original repository](https://github.com/amazon-science/patchcore-inspection)
+
+As expected, scores fluctuate slightly due to the random first element in subsampling. However, the results are consistent with those reported in the paper.
+
+![Benchmark for Patchcore 1%](results.svg "AUROC SCore Benchmark for Patchcore 1%")
+| Key | Mean Value | Max Value | Min Value |
+|-----|------------|-----------|-----------|
+| cable | 99.6% | 99.9% | 99.2% |
+| bottle | 100.0% | 100.0% | 100.0% |
+| hazelnut | 100.0% | 100.0% | 100.0% |
+| wood | 99.2% | 99.4% | 99.0% |
+| metal_nut | 100.0% | 100.0% | 99.9% |
+| screw | 96.1% | 96.9% | 94.3% |
+| pill | 95.5% | 97.2% | 94.2% |
+| transistor | 100.0% | 100.0% | 99.9% |
+| tile | 99.1% | 99.5% | 98.9% |
+| grid | 98.3% | 98.8% | 97.7% |
+| zipper | 99.1% | 99.2% | 98.8% |
+| carpet | 97.8% | 98.4% | 97.3% |
+| toothbrush | 99.9% | 100.0% | 99.4% |
+| leather | 100.0% | 100.0% | 100.0% |
+| capsule | 98.6% | 99.0% | 97.7% |
+
+---
+
+# Final Comments
+
+I implemented an aggregation function based on average pooling, which I personally find aligns more closely with the description in the paper compared to the original repository’s approach. Additionally, it's more transparent, as the workings of adaptive average pooling aren't immediately clear. Note that when using the `AverageAggregator`, the optimal patch size may differ from the values suggested in the paper.
--- src/example.py
+++ src/example.py
@@ -1,27 +1,16 @@
 from sklearn.metrics import roc_auc_score
 import os
 from patchcore import model_generation
-from patchcore.models import AverageAggregator, OriginalAggregator
 from tqdm import tqdm
 import numpy as np
 from PIL import Image
-from random import shuffle
 
-def getFilesinDir(directory):
-    image_files = []
-    for subdir, dirs, files in os.walk(directory):
-        for file in files:
-            path_to_subdir = os.path.join(directory,subdir)
-            path_to_file = os.path.join(path_to_subdir, file)
-            if os.path.isfile(path_to_file):
-                image_files.append(path_to_file)
-    return image_files   
 
 mvtec_dir = "/home/xaver/Downloads/mvtec_anomaly_detection/"
 batch_size = 1
 device="cpu"
 
-
+# used to retrieve training data
 def getFilesinDir(directory):
     image_files = []
     for subdir, dirs, files in os.walk(directory):
@@ -34,73 +23,73 @@ def getFilesinDir(directory):
 
 
 
-for subsampling_percentage in [1,10,25]:
+for subsampling_percentage in [1,10]:
 
+  #set up logging
   resultfile = os.path.join("./",f"results_{subsampling_percentage}.txt")
   with open(resultfile,"w") as resFile:
       resFile.write("Dataset \t  Score \n")
 
+  #set up logging
   datasets = os.listdir(mvtec_dir)
-  shuffle(datasets)
   for dataset in datasets:
-      if not os.path.isdir(os.path.join(mvtec_dir,dataset)):
-          continue
-      directory = os.path.join(mvtec_dir,f"{dataset}/train")
-      image_files = getFilesinDir(directory)
+    if not os.path.isdir(os.path.join(mvtec_dir,dataset)):
+        continue
+    directory = os.path.join(mvtec_dir,f"{dataset}/train")
+    image_files = getFilesinDir(directory)
 
-      #model creatio
+#cropping as WideResNet50 imagenetv1 proposes
+    crop_shape=[]
+    with Image.open(image_files[0]) as im:
+        height,width = im.size
+        assert height == width, "images must be square"
+        crop = height * (1-224/256.0)
+        crop = int(crop/2)
+        crop_shape = (crop,crop,width-crop,height-crop)
 
+    scorer,loader = model_generation.train_patchcore( image_files,
+                                                    n_percent=subsampling_percentage,
+                                                    batch_size=batch_size,
+                                                    device=device,
+                                                    crop_tuple=crop_shape,
+                                                    target_dim=None)
 
-      crop_shape=[]
-      #cropping as WideResNet50 imagenetv1 proposes
-      with Image.open(image_files[0]) as im:
-          height,width = im.size
-          assert height == width, "images must be square"
-          crop = height * (1-224/256.0)
-          crop = int(crop/2)
-          crop_shape = (crop,crop,width-crop,height-crop)
+    directory = os.path.join(mvtec_dir,f"{dataset}/test")
+    image_files = getFilesinDir(directory)
 
-      scorer,loader = model_generation.train_patchcore(image_files,
-                                                          n_percent=subsampling_percentage,
-                                                          batch_size=batch_size,
-                                                          device=device,
-                                                          crop_tuple=crop_shape,
-                                                          target_dim=None)
+    batched_files = [image_files[i:i + batch_size]
+                        for i in range(0, len(image_files), batch_size)
+    ]
+    score = []
 
-      directory = os.path.join(mvtec_dir,f"{dataset}/test")
-      image_files = getFilesinDir(directory)
+    #i you want to sweep over different nearest neighbour values set them here
+    b_vals = [1]
+    for b in b_vals:
+        scorer.set_b(b)
+        scores = [
+            scorer(loader.load_images_as_tensor(x))
+            for x in tqdm(batched_files,"scoring images",leave=False)
+        ]
+        image_scores = [
+            x.numpy(force=True)
+            for batch in scores
+            for x in batch[0]
+        ]
+        pixel_scores = np.array([
+            x.numpy(force=True)
+            for batch in scores
+            for x in batch[1]
+        ])
 
-      batched_files = [image_files[i:i + batch_size]
-                          for i in range(0, len(image_files), batch_size)
-      ]
-      score = []
-      b_vals = [1]
-      for b in b_vals:
-          scorer.set_b(b)
-          scores = [
-              scorer(loader.load_images_as_tensor(x))
-              for x in tqdm(batched_files,"scoring images",leave=False)
-          ]
-          image_scores = [
-              x.numpy(force=True)
-              for batch in scores
-              for x in batch[0]
-          ]
-          pixel_scores = np.array([
-              x.numpy(force=True)
-              for batch in scores
-              for x in batch[1]
-          ])
+        mask = ["good" not in x for x in image_files]
+        score.append(roc_auc_score(mask,image_scores))
 
-          mask = ["good" not in x for x in image_files]
-          score.append(roc_auc_score(mask,image_scores))
+    #save best performing results
+    idx = np.argmax(score)
+    score = score[idx]
+    b = b_vals[idx]
 
-
-      idx = np.argmax(score)
-      score = score[idx]
-      b = b_vals[idx]
-
-      #save results
-      print(f"score in {dataset} is {score} for b = {b}")
-      with open(resultfile,"a") as resFile:
-          resFile.write(f"score in {dataset} is {score} for b = {b}, p= {3} \n")
+    #save results
+    print(f"score in {dataset} is {score} for b = {b}")
+    with open(resultfile,"a") as resFile:
+        resFile.write(f"score in {dataset} is {score} for b = {b}, p= {3} \n")
